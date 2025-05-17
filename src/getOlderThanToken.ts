067f3e2e9027a6dc34eae5d91b6b27234297a1ce
Mike Hodnick <mike@kindohm.com>
2025-05-16 21:25:38 -0500

    change time to /1000

--- src/getOlderThanToken.ts
+++ src/getOlderThanToken.ts
@@ -6,6 +6,7 @@ export const getOlderThanToken = (summary: CollectionSummary) => {
   const { item_type, item_id } =
     summary.collection_summary.tralbum_lookup[firstKey];
   const now = new Date();
-  const olderThanToken = `${now.getTime()}:${item_id}:${item_type}::`;
+  const time = Math.floor(now.getTime() / 1000);
+  const olderThanToken = `${time}:${item_id}:${item_type}::`;
   return olderThanToken;
 };
